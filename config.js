c43d099c2f92a807fb49e0daf443f5171d6df73d
José Antonio <9931324+joseantonnio@users.noreply.github.com>
2023-07-26 20:07:38 -0300

    add scrap url

--- config.js
+++ config.js
@@ -3,14 +3,13 @@ require('dotenv').config()
 let config = {}
 
 config.urls = [
-    'url1',
-    'url2'
+    'https://www.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios/flex/estado-sp/regiao-de-ribeirao-preto/regiao-de-sao-carlos?cf=1&ctp=2&ctp=9&ctp=1&ctp=5&pe=40000&rs=30&sf=1&cad=2&fncs=1'
 ]
 
 // this tool can help you create the interval string:
 // https://tool.crontap.com/cronjob-debugger
 
-config.interval = '*/5 * * * *' 
+config.interval = '*/5 * * * *'
 config.telegramChatID = process.env.TELEGRAM_CHAT_ID
 config.telegramToken = process.env.TELEGRAM_TOKEN
 config.logFile = 'scrapper.log'
